da6078cb053991352affe04e374a88aefd22a116
Issac-code <eshaqjabbari_dev@outlook.com>
2023-07-31 23:17:43 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import RightSide from "./Components/RightSide";
+import Main from "./Components/Main";
+import LeftSide from "./Components/LeftSide";
+
+import './App.css';
+
+const App = () => {
+    return (
+        <>
+            <RightSide/>
+            <Main/>
+            <LeftSide/>
+        </>
+    );
+};
+
+export default App;
